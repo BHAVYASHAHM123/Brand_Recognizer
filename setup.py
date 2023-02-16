d3881c5544e35414ce7a1820c6aac2aaf43d4191
BHAVYASHAHM123 <bhavya789shah@gmail.com>
2023-02-16 12:27:45 +0530

    Update setup data

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "FSDS_NOV_deepCNNClassifier"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "Brand_Recognizer"
+AUTHOR_USER_NAME = "BHAVYASHAHM123"
 SRC_REPO = "deepClassifier"
-AUTHOR_EMAIL = "sunny.c17hawke@gmail.com"
+AUTHOR_EMAIL = "bhavya789shah@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
+    description="Brand Recognizer app",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
